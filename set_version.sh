536b4fca3b227e1dc0b855103da561f71a1b4a5b
mk202205 <105193079+mk202205@users.noreply.github.com>
2022-07-21 12:04:43 +0200

    Fix pattern

--- set_version.sh
+++ set_version.sh
@@ -3,8 +3,12 @@
 new_version="$1"
 echo "Updating version to '$new_version' ..."
 
-sed -i -e "s/1.[0-9.]+-alpha-dev/${new_version}/g" \
+sed -i -e "s/1\.[0-9.]\+-alpha-dev/${new_version}/g" \
   src/a/extra.txt \
   main.txt
 
 echo "Done."
+
+echo "Test:"
+cat src/a/extra.txt
+echo "Test done."
