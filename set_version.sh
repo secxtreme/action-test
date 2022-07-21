d8492934351008db2afce5d87783497510576bba
mk202205 <105193079+mk202205@users.noreply.github.com>
2022-07-21 11:06:13 +0200

    Add prepare script

---
+++ set_version.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+new_version="$1"
+echo "Updating version to '$new_version' ..."
+
+sed -i -e "s/1.[0-9.]+-alpha-dev/${new_version}/g" \
+  src/a/extra.txt \
+  main.txt
+
+echo "Done."
